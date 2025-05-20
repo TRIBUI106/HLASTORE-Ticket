f8dc386b0b1de35f89b203045bc762b1f3f6f501
chez1s <toptrainghieml@gmail.com>
2025-05-20 20:30:35 +0700

    Remove config

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  token:
-    "MTM0NjkxNDM1ODYyMTM3MjQ2OA.GlfRxu.EhzNyXCTlq1_Rxdmypwm_GWLxVp6O-EfrNuza4",
-  roleSupport: ["1346919323754827878", "1359148431968567460"],
-  prefix: "helaticket!",
-  ownerId: "1250225950268850312",
-  ticketCategory: "1346908778855141477",
-  closedTicketCategory: "1346912722851201114",
-};
